ac56e2e2bf86900c6ce1aa81024ca5fc898eb909
Ignat Mustafin <ignatmustafin77@gmail.com>
2024-11-07 23:16:26 +0300

    #19 changed FILE API URL

--- packages/webapp/src/Utils/GetPreviewSrc.ts
+++ packages/webapp/src/Utils/GetPreviewSrc.ts
@@ -1,4 +1,4 @@
-const FILE_API_URL = import.meta.env.VITE_API_URL;
+const FILE_API_URL = import.meta.env.VITE_FILE_API_URL;
 
 const getPreviewSrc = (previewUrl?: string) =>
   previewUrl ? `${FILE_API_URL}${previewUrl}` : undefined;
