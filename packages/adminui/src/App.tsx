1b09d093ad987643fdd730b5ecaa0a5a0d421335
RunOrFight <dragoinfury@gmail.com>
2025-02-01 12:59:51 +0300

    adminui wip

--- packages/adminui/src/App.tsx
+++ packages/adminui/src/App.tsx
@@ -1,7 +1,31 @@
 import "./App.css";
-import { Layout, Menu, MenuProps } from "antd";
+import {
+  ConfigProvider,
+  ConfigProviderProps,
+  Flex,
+  Layout,
+  Menu,
+  MenuProps,
+  theme as antdTheme,
+} from "antd";
+import ru from "antd/locale/ru_RU";
+import { CSSProperties } from "react";
 
-const LAYOUT_STYLE = { minHeight: "100dvh" };
+const APP_CONFIG: ConfigProviderProps = {
+  locale: ru,
+  theme: {
+    algorithm: antdTheme.defaultAlgorithm,
+    cssVar: { key: "adminui" },
+    hashed: false,
+    components: {
+      Layout: {
+        headerHeight: 56,
+      },
+    },
+  },
+};
+
+const LAYOUT_STYLE: CSSProperties = { minHeight: "100dvh" };
 
 type TMenuItem = Required<MenuProps>["items"][number];
 
@@ -12,22 +36,37 @@ const MENU_ITEMS: TMenuItem[] = [
   },
 ];
 
+const MAIN_STYLE: CSSProperties = {
+  padding: 24,
+};
+
+const CONTENT_STYLE: CSSProperties = {
+  background: "var(--ant-color-bg-container)",
+  height: "100%",
+  borderRadius: "var(--ant-border-radius-lg)",
+  padding: 24,
+};
+
 const App = () => {
   return (
-    <Layout style={LAYOUT_STYLE}>
-      <Layout.Sider width="25%">
-        <Menu
-          theme="dark"
-          defaultSelectedKeys={["1"]}
-          mode="inline"
-          items={MENU_ITEMS}
-        />
-      </Layout.Sider>
-      <Layout>
-        <Layout.Header>Header</Layout.Header>
-        <Layout.Content>Content</Layout.Content>
+    <ConfigProvider {...APP_CONFIG}>
+      <Layout style={LAYOUT_STYLE}>
+        <Layout.Header></Layout.Header>
+
+        <Layout>
+          <Layout.Sider width="200px" theme={"light"}>
+            <Menu
+              defaultSelectedKeys={["1"]}
+              mode="inline"
+              items={MENU_ITEMS}
+            />
+          </Layout.Sider>
+          <Layout.Content style={MAIN_STYLE}>
+            <Flex style={CONTENT_STYLE}>{"Contet"}</Flex>
+          </Layout.Content>
+        </Layout>
       </Layout>
-    </Layout>
+    </ConfigProvider>
   );
 };
 
