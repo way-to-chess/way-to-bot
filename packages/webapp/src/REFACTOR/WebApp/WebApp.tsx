171bcf447c6fbbfe73f4d94704cbda9a00a5d2ea
Aliaksandr Mikhniuk <dragoinfury@gmail.com>
2025-05-15 23:30:19 +0300

    wip

--- packages/webapp/src/REFACTOR/WebApp/WebApp.tsx
+++ packages/webapp/src/REFACTOR/WebApp/WebApp.tsx
@@ -3,27 +3,10 @@ import {WEB_APP_ROUTER} from "./Routes";
 import {Provider} from "react-redux";
 import "../../Assets/Style/Global.css";
 import {store} from "../Store";
-import {createContext} from "react";
 
 if (!Telegram.WebApp || Telegram.WebApp.platform === "unknown") {
     document.body.setAttribute("data-dev", "true");
 }
-
-interface IAuthContext {
-    telegram: typeof Telegram.WebApp.initDataUnsafe.user;
-    user: unknown;
-}
-
-const AuthContext = createContext({
-    telegram: null,
-    user: null,
-});
-
-const AuthContextProvider = ({children}) => {
-    const value = {};
-
-    return <AuthContext>{children}</AuthContext>;
-};
 
 const WebApp = () => (
     <Provider store={store}>
