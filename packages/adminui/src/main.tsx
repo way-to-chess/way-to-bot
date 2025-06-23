032d034071ddc8256068cbe3e82399083ad71d62
Aliaksandr Mikhniuk <dragoinfury@gmail.com>
2025-06-23 19:46:42 +0300

    admin password

--- packages/adminui/src/main.tsx
+++ packages/adminui/src/main.tsx
@@ -4,16 +4,32 @@ import {Provider} from "react-redux";
 import {store} from "./Store/Store";
 import {RouterProvider} from "react-router";
 import {ROUTER} from "./Router";
-import {FC, PropsWithChildren} from "react";
+import {FC, PropsWithChildren, useLayoutEffect, useState} from "react";
 import {authApi} from "@way-to-bot/shared/redux/authApi";
 import {Skeleton} from "antd";
 import '@ant-design/v5-patch-for-react-19';
 
 
 const WithAuth: FC<PropsWithChildren> = ({children}) => {
-    const {isLoading, isError} = authApi.useAuthByTelegramQuery({
-        tgId: 409658449,
-    })
+    const [tgId, setTgId] = useState(() => localStorage.getItem("tgId") ? Number(localStorage.getItem("tgId")) : 0)
+
+    const {isLoading, isError} = authApi.useAuthByTelegramQuery({tgId})
+
+    useLayoutEffect(() => {
+        new Promise<string | number | null>((resolve) => {
+            const value = tgId || window.prompt("Пароль")
+
+            resolve(value)
+        }).then((value) => {
+            if (!value) {
+                return
+            }
+            
+            setTgId(Number(value))
+            localStorage.setItem("tgId", String(value))
+        })
+    }, []);
+
 
     if (isLoading) {
         return <Skeleton/>
