724e0b5ce0bec86bd59dc18557a1156b401b5172
Aliaksandr Mikhniuk <dragoinfury@gmail.com>
2025-06-30 22:54:04 +0300

    support messages for user ids

--- packages/adminui/src/Store/TG/TGApi.ts
+++ packages/adminui/src/Store/TG/TGApi.ts
@@ -1,17 +1,16 @@
-import {adminApi} from "../AdminApi";
-import {TAdminTgSendCustomMessagePayload} from "@way-to-bot/shared/api/zod/admin/tg.schema";
+import { adminApi } from "../AdminApi";
+import { TAdminTgSendCustomMessagePayload } from "@way-to-bot/shared/api/zod/admin/tg.schema";
 
 const TGApi = adminApi.injectEndpoints({
-    endpoints: (build) => ({
-        sendCustomMessage: build.mutation<void, TAdminTgSendCustomMessagePayload>({
-            query: (payload) => ({
-                url: "tg/message/custom",
-                method: "POST",
-                body: payload,
-            }),
-        }),
+  endpoints: (build) => ({
+    sendCustomMessage: build.mutation<void, TAdminTgSendCustomMessagePayload>({
+      query: (payload) => ({
+        url: "tg/message/custom",
+        method: "POST",
+        body: payload,
+      }),
+    }),
+  }),
+});
 
-    })
-})
-
-export {TGApi}
+export { TGApi };
