2e85e17eb9d703b28a534e367122bc0f40870cc0
Aliaksandr Mikhniuk <dragoinfury@gmail.com>
2025-08-03 13:41:02 +0300

    webapp - vite config

--- packages/webapp/vite.config.https.ts
+++ packages/webapp/vite.config.https.ts
@@ -1,24 +1,25 @@
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
 import * as fs from "fs";
-import { resolve } from "node:path";
+import {resolve} from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: "secure.localhost",
-    https: {
-      key: fs.readFileSync(".cert/key.pem").toString(),
-      cert: fs.readFileSync(".cert/cert.pem").toString(),
+    plugins: [react()],
+    server: {
+        host: "secure.localhost",
+        port: 5555,
+        https: {
+            key: fs.readFileSync(".cert/key.pem").toString(),
+            cert: fs.readFileSync(".cert/cert.pem").toString(),
+        },
     },
-  },
-  resolve: {
-    alias: [
-      {
-        find: "@way-to-bot/shared",
-        replacement: resolve(__dirname, "../shared/src"),
-      },
-    ],
-  },
+    resolve: {
+        alias: [
+            {
+                find: "@way-to-bot/shared",
+                replacement: resolve(__dirname, "../shared/src"),
+            },
+        ],
+    },
 });
